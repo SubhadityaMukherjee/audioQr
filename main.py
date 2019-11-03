68ad2d5cbf9e734bfe26091226cc22ca219cb8f1
Subhaditya <msubhaditya@gmail.com>
2019-11-03 22:38:33 +0530

    Finis

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from scipy.signal import find_peaks, peak_widths
 from scipy.io.wavfile import write
 import random
 import sounddevice as sd
+import soundfile as sf
 import datetime
 
 fs = 44100  # frequency
 chno = 2  # no of channels
 duration = 2
-f = 440.0 # sine freq
+f = 440.0  # sine freq
 
 
 def rem_empty(wave):
@@ -23,20 +24,33 @@ def rem_empty(wave):
     return np.array(temp)
 
 
+def rem_empty_2(wave):
+    return np.array([x for x in wave if x != 0.0])
+
+
 # p = [[-0., -0.], [-0., -0.], [-0., -0.], [-0.00051845, -0.00051845],
 #      [-0.00040478, -0.00040478], [-0.00033682, -0.00033682]]
 
 # print(rem_empty(p))
 
 
-def gen_wave():
-    now = datetime.datetime.now()
-    new_f = f+now.hour%10
-    created_wave = (np.sin(2*np.pi*np.arange(fs*1)*new_f/fs)).astype(np.float32)
+def gen_wave(test=0):
+    if test == 1:
+        new_f = f + 22 % 10
+    if test == 2:
+        new_f = f + 23 % 10
+    else:
+        now = datetime.datetime.now()
+        new_f = f + now.hour % 10
+    created_wave = (np.sin(2 * np.pi * np.arange(fs * 1) * new_f / fs)).astype(
+        np.float32)
     write('gen.wav', fs, created_wave)
+    return created_wave
 
 
-# gen_wave()
+def open_wav():
+    data, fs = sf.read("gen.wav", dtype='float32')
+    return rem_empty(data)
 
 
 def recorder():
@@ -58,40 +72,35 @@ def recorder():
 # recorder()
 
 
-def wave_peaker(data):
-    peaks, _ = find_peaks(data)
-    widths_all = peak_widths(data, peaks)
-
-    plt.plot(peaks, "x")
+def wave_peaker(data, fn):
+    print(data)
+    plt.plot(data)
     plt.xlim(0, 1500)
     # plt.ylim(0.005, -0.005)
-    plt.savefig("peakers.png")
-    return [peaks, widths_all]
+    plt.savefig("{}.png".format(fn))
 
 
 # Using a difference threshold and counting q of peaks
 def comparison_fast(data1, data2):
-    flag = 0
-    p1, w1 = wave_peaker(data1)
-    p2, w2 = wave_peaker(data2)
+    len1 = len(data1)
+    comp = rem_empty_2(data1) - rem_empty_2(data2)
 
-    for p in [p1, p2, w1, w2]:
-        p = np.array(p)
+    comp = np.array([int(x) for x in comp])
+    print(comp)
+    comp = np.array([x for x in comp if x == 0])
+    print("error within the range of {} frames".format(len1 - len(comp)))
 
-    peak_sub = p1 - p2
-    width_sub = w1 - w2
 
-    # no of peaks
+def main_without_record_gen():
+    data1 = open_wav()
+    print("[INFO] opened wave")
+    data2 = gen_wave(2)
+    print("[INFO] generated wave")
+    wave_peaker(data1, "open_peak")
+    print("[INFO] Wave 1 peak")
+    wave_peaker(data2, "gen_peak")
+    print("[INFO] Wave 2 peak")
+    comparison_fast(data1, data2)
+    print("[INFO] Comparison done")
 
-    lenp1 = len(p1)
-    lenp2 = len(p2)
-
-    if (lenp1 >= lenp2 + 3 and lenp1 <= lenp2 - 3):
-        flag = 1
-    else:
-        flag = 0
-
-    # difference
-
-    peak_sub = [x < threshold_comp for x in peak_sub]
-    # width_sub = [x<threshold_comp for x in width_sub]
+main_without_record_gen()
