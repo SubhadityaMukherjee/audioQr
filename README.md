070c0c6b2ab3ec5966829ad61910c24386b66ea5
Subhaditya <msubhaditya@gmail.com>
2019-11-19 13:02:13 +0530

    changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# README 
+ - This program is implements a form of Near Sound Data transfer
+ - It uses pyaudio to record a sound
+ - Generates a numpy array based on conditions
+ - Run using main.py
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import sys
-from scipy.signal import find_peaks, peak_widths
+# from scipy.signal import find_peaks, peak_widths
 from scipy.io.wavfile import write
 import random
 import sounddevice as sd
@@ -42,7 +42,7 @@ def gen_wave(test=0):
     else:
         now = datetime.datetime.now()
         new_f = f + now.hour % 10
-    created_wave = (np.sin(2 * np.pi * np.arange(fs * 1) * new_f / fs)).astype(
+    created_wave = (np.sin(2 * np.pi * np.arange(fs) * new_f / fs)).astype(
         np.float32)
     write('gen.wav', fs, created_wave)
     return created_wave
@@ -69,9 +69,6 @@ def recorder():
     return fin_wave
 
 
-# recorder()
-
-
 def wave_peaker(data, fn):
     print(data)
     plt.plot(data)
@@ -80,7 +77,6 @@ def wave_peaker(data, fn):
     plt.savefig("{}.png".format(fn))
 
 
-# Using a difference threshold and counting q of peaks
 def comparison_fast(data1, data2):
     len1 = len(data1)
     comp = rem_empty_2(data1) - rem_empty_2(data2)
@@ -94,7 +90,7 @@ def comparison_fast(data1, data2):
 def main_without_record_gen():
     data1 = open_wav()
     print("[INFO] opened wave")
-    data2 = gen_wave(2)
+    data2 = gen_wave(1)
     print("[INFO] generated wave")
     wave_peaker(data1, "open_peak")
     print("[INFO] Wave 1 peak")
@@ -102,5 +98,6 @@ def main_without_record_gen():
     print("[INFO] Wave 2 peak")
     comparison_fast(data1, data2)
     print("[INFO] Comparison done")
+
 
 main_without_record_gen()
